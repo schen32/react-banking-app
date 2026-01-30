71c1823015b030dfb27831fa393f649c42988c75
Simon Chen <simonc8192@gmail.com>
2026-01-29 21:02:25 -0800

    enhance SignUp and AuthForm: implement user retrieval on sign-up and improve sign-in functionality

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -22,7 +22,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { signIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -56,13 +56,13 @@ const AuthForm = ({ type }: { type: string }) => {
         const newUser = await signUp(data);
         setUser(newUser);
       } else if (type === "sign-in") {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
-        // if (response) {
-        //   router.push("/");
-        // }
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) {
+          router.push("/");
+        }
       }
     } catch (error) {
     } finally {
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,9 +5,12 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
     try {
-        
+        const { account } = await createAdminClient();
+
+        const response = await account.createEmailPasswordSession({ email: email, password: password });
+        return parseStringify(response);
     } catch (error) {
         console.error("Error", error)
     }
@@ -46,7 +49,8 @@ export const signUp = async ( userData: SignUpParams ) => {
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
-      return await account.get();
+      const user = await account.get();
+      return parseStringify(user);
     } catch (error) {
       return null;
     }
